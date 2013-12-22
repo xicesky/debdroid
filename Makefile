0ca6b96d456b1477772529c00b5e72bb0ae5fe19
Markus Dangl <markus@q1cc.net>
2013-12-22 11:55:36 +0100

    Initial, debootstrap sucks

---
+++ Makefile
@@ -0,0 +1,189 @@
+
+# This is a GNU Make Makefile
+# Written for GNU Make 3.82
+# (c) 2013 Markus Dangl <sky@q1cc.net>
+# Licensed under the GPL (However you got your hands on this source file)
+
+###################################################################################################
+# Usage (once customized):
+# 1. Create the initial "Makefile":
+#       > ...shell$ make -f Makefile.make Makefile
+#	# The Makefile will automatically be updated in the future.
+# 2. Build the application or a specific file:
+#       > ...shell$ make
+#       > ...shell$ make specific-file
+# 3. Clean up
+#	# To clean up intermediate files
+#       > ...shell$ make clean
+#	# To clean up all generated files (e.g. before RCS checkin or src distribution)
+#       > ...shell$ make distclean
+
+###################################################################################################
+# Configuration of installable debian image
+#
+#
+
+ARCH := armhf
+RELEASE := jessie
+VARIANT := minbase
+MIRROR := http://ftp2.de.debian.org/debian
+IMAGE_SIZE_MB := 512
+IMAGE_SIZE_BYTES := $(shell expr ${IMAGE_SIZE_MB} \* 1024 \* 1024)	# TODO make can calc, right?
+
+export ARCH RELEASE VARIANT MIRROR IMAGE_SIZE_MB IMAGE_SIZE_BYTES
+
+SUDO := $(shell which sudo) -E
+export SUDO
+
+###################################################################################################
+
+# Files
+SOURCE_DIR = .
+# Warning: Target and build directory get removed by clean & co
+TARGET_DIR = target
+BUILD_DIR = build
+CACHE_DIR = cache
+MARKER = $(BUILD_DIR)/marker
+
+# Targets
+DEBDROID = $(BUILD_DIR)/debdroid
+DEBDROID_TARBALL = $(TARGET_DIR)/debdroid.tar.gz
+FSIMAGE=$(BUILD_DIR)/fsimage.bin
+FULL_IMAGE=$(TARGET_DIR)/fsimage.bin
+TARGETS = $(DEBDROID_TARBALL) $(FULL_IMAGE)
+ALL_TARGETS = $(TARGETS)
+
+SCRIPT = $(shell readlink -f ./helper.sh)
+
+# User
+REALUSER = $(shell id -un)
+export REALUSER
+
+# Strings
+INFO_CLEAN = $(info )$(info Cleaning up...)$(info )
+INFO_MAKEFILE = $(info )$(info Updating Makefile...)
+
+INFO_BUILD = $(info Building $(1)...)
+
+# OS detection
+UNAME = $(shell uname)
+#$(info OS: $(OS) $(UNAME))
+ifneq (,$(findstring CYGWIN,$(UNAME)))
+    OSMODE=cygwin
+    FU_SEP=/
+    RM=rm -rf
+	RMDIR=rmdir
+    CP=cp
+	MV=mv
+	LN=ln -s
+    MKDIR=mkdir -p
+    SHARED_LIBRARY_SUFFIX=.dll
+else ifeq ($(OS),Windows_NT)
+	# THIS SCRIPT WILL NEVER RUN UNDER WINDOWS U KNOW
+    OSMODE=windows
+    FU_SEP=\\
+    RM=rd /S /Q
+	RMDIR=rd
+    CP=copy >nul
+	MV=?? # TODO
+	LN=?? # WONT EVER WORK
+    MKDIR=mkdir 2>nul
+    SHARED_LIBRARY_SUFFIX=.dll
+else
+    OSMODE=unix
+    FU_SEP=/
+    RM=rm -rf
+	RMDIR=rmdir
+    CP=cp
+	MV=mv
+	LN=ln -s
+    MKDIR=mkdir -p
+    SHARED_LIBRARY_SUFFIX=.so
+endif
+#$(info Using $(OSMODE) style commands and paths.)
+
+.PHONY: default clean-rooted clean dist-clean distclean all info checks
+.PRECIOUS: $(DEBDROID) $(DEBDROID).tar.gz
+
+default: all
+#default: info
+
+clean-rooted: ; $(INFO_CLEAN)
+	-$(SUDO) $(RM) $(DEBDROID)
+	# Just to be sure
+	-$(SUDO) $(RM) $(DEBDROID).tar.gz
+
+clean: clean-rooted ; $(INFO_CLEAN)
+	-$(RM) $(BUILD_DIR)
+
+distclean dist-clean: clean; $(INFO_CLEAN)
+	-$(RM) $(TARGET_DIR)
+
+all: $(ALL_TARGETS)
+
+$(BUILD_DIR) $(TARGET_DIR) $(CACHE_DIR) $(MARKER):
+	$(MKDIR) $@
+
+$(MARKER): | $(TARGET)
+
+info: checks
+	$(SCRIPT) info
+
+checks:
+	$(SCRIPT) checks
+
+###################################################################################################
+
+#$(BUILD_DIR)/%.temp: $(SOURCE_DIR)/%.srcfile $(BUILD_DIR)
+#	@executable-preprocess-source --some-parameter $< > $@
+
+#$(TARGET_DIR)/%.bin: $(BUILD_DIR)/%.temp $(TARGET_DIR) ; $(call INFO_BUILD,$(notdir $@))
+#	@executable-compile-binary --some-parameter -o $@ $<
+
+# Pre-download debootstrap packages
+
+CACHEFILE = $(CACHE_DIR)/debootstrap-$(RELEASE)-$(ARCH).tar.gz
+$(CACHEFILE): | $(CACHE_DIR)
+	$(SCRIPT) bootstrap-download $@
+
+$(DEBDROID): $(CACHEFILE) | $(BUILD_DIR) checks
+	-$(SUDO) $(RM) $(DEBDROID)
+	$(SCRIPT) bootstrap $@ $<
+
+$(DEBDROID).tar.gz: $(DEBDROID) ; $(call INFO_BUILD,$(notdir $@))
+	-$(SUDO) $(RM) $(DEBDROID).tar.gz
+	$(SCRIPT) tarball $< $@
+
+$(DEBDROID_TARBALL): $(DEBDROID).tar.gz | $(TARGET_DIR) ; $(call INFO_BUILD,$(notdir $@))
+	$(MV) $< $@
+	$(LN) ../$@ $<
+
+$(FSIMAGE): $(DEBDROID) | $(BUILD_DIR)
+	-rm $@
+	dd if=/dev/zero bs=1M count=${IMAGE_SIZE_MB} | pv -s ${IMAGE_SIZE_BYTES} > $@
+	$(SCRIPT) format-fsimage "$@"
+
+$(FULL_IMAGE): $(FSIMAGE) $(DEBDROID) | $(TARGET_DIR)
+	-rm $@
+	$(SCRIPT) loop-mount "$<" "$(BUILD_DIR)/mnt"
+	$(SCRIPT) copy-content "$(DEBDROID)" "$(BUILD_DIR)/mnt"
+	
+	@# TODO should be optional
+	@echo "--------------------------------------------------------------------------------"
+	@echo "Pausing script here to allow for custom modifications."
+	@echo "The current filesystem is mounted at: $(BUILD_DIR)/mnt"
+	@echo "Press return to continue."
+	@read DUMMY
+	@echo "--------------------------------------------------------------------------------"
+	
+	$(SCRIPT) umount "$(BUILD_DIR)/mnt"
+	$(RMDIR) "$(BUILD_DIR)/mnt"
+	@# Linux _should_ destroy the loopback device automatically
+	$(MV) $< $@
+	$(LN) ../$@ $<
+
+###################################################################################################
+
+Makefile: Makefile.make ; $(INFO_MAKEFILE)
+	@$(CP) $< $@
+
---
+++ helper.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+
+ARCH=${ARCH:-"armel"}
+RELEASE=${RELEASE:-"jessie"}
+VARIANT=${VARIANT:-"minbase"}
+MIRROR=${MIRROR:-"http://ftp2.de.debian.org/debian"}
+
+IMAGE_SIZE_MB=${IMAGE_SIZE_MB:-"512"}
+IMAGE_SIZE_BYTES="`expr $IMAGE_SIZE_MB \* 1024 \* 1024`"
+
+REALUSER=${REALUSER:-"`id -un`"}
+VERBOSE=${VERBOSE:-"false"}
+SUDO=${SUDO:-"`which sudo` -E"}
+
+export ARCH RELEASE VARIANT MIRROR REALUSER VERBOSE SUDO
+
+amiroot() {
+    test "`id -u`" = "0"
+}
+
+invoke() {
+    echo "$@"
+    "$@"
+}
+
+wrap_bootstrap() {
+    invoke debootstrap \
+        --foreign --arch="$ARCH" \
+        --variant="$VARIANT" \
+        "$@"
+}
+
+MODE="$1"
+shift
+
+case $MODE in
+    checks)
+        if ! amiroot ; then
+            $SUDO "$0" "$MODE" "$@"
+        else
+            # TODO Check for debootstrap
+            ! $VERBOSE || echo "Checks successful." # We avoid negative returns by using "! $VERBOSE ||"
+        fi
+        ;;
+
+    bootstrap-download)
+        DEST="$1"
+        [ -z "$DEST" ] && DEST="cache/debootstrap-${RELEASE}-${ARCH}.tar.gz"
+        [ -e "$DEST" ] && { echo "Destination file or directory exists: \"$DEST\"" 1>&2 ; exit 1; }
+        # EVEN THE FUCKING DOWNLOADER NEEDS TO BE ROOT?
+        # FU debootstrap!
+        if ! amiroot ; then
+            $SUDO "$0" "$MODE" "$@"
+        else
+            TEMPDIR=`mktemp -d build/debootstrap.XXXXXXXX`
+            wrap_bootstrap --verbose --make-tarball "$DEST" "$RELEASE" "$TEMPDIR" "$MIRROR"
+            RESULT="$?"
+            rm -r "$TEMPDIR"
+            # When debootstrap fails it _sometimes_ leaves us with a dest file anyway...
+            if [ ! "$RESULT" -eq "0" ] ; then
+                echo "debootstrap failed." 1>&2
+                rm "$DEST"  # Clean up on error
+                exit $RESULT
+            fi
+            set -e
+            chmod 660 "$DEST"
+            chown $REALUSER "$DEST"
+            set +e
+        fi
+        ;;
+
+    bootstrap)
+        DEST="$1"
+        [ -e "$DEST" ] && { echo "Destination file or directory exists: \"$DEST\"" 1>&2 ; exit 1; }
+        if ! amiroot ; then
+            $SUDO "$0" "$MODE" "$@"
+        else
+            mkdir "$DEST" || exit 1
+            # Check if we have a cached bootstrap thingy
+            CACHED="$2"
+            [ -z "$CACHED" ] && CACHED="cache/deboostrap-${RELEASE}-${ARCH}.tar.gz"
+
+            if [ -e "$CACHED" ] ; then
+                wrap_bootstrap --verbose --unpack-tarball "$CACHED" "$RELEASE" "$DEST"
+            else
+                wrap_bootstrap --verbose "$RELEASE" "$DEST" "$MIRROR"
+            fi
+
+            RESULT="$?"
+            [ "$RESULT" -eq "0" ] || rm -r "$DEST"  # Clean up on error
+            exit $RESULT
+        fi
+        ;;
+
+    tarball)
+        SOURCE="$1"
+        DEST="$2"
+        [ -z "$DEST" ] && DEST="`basename $SOURCE`.tar.gz"
+        [ -d "$SOURCE" ] || { echo "Source directory missing: \"$SOURCE\"" 1>&2 ; exit 1; }
+        [ -e "$DEST" ] && { echo "Destination file exists: \"$DEST\"" 1>&2 ; exit 1; }
+
+        if ! amiroot ; then
+            $SUDO "$0" "$MODE" "$@"
+        else
+            set -e
+            tar cvzf "$DEST" "$SOURCE"
+            chmod 660 "$DEST"
+            chown "$REALUSER" "$DEST"
+            set +e
+        fi
+        ;;
+
+    format-fsimage)
+        IMAGE="$1"
+        FSTYPE=ext4
+        if ! amiroot ; then
+            $SUDO "$0" "$MODE" "$@"
+        else
+            set -e
+            DEV=`losetup -f`
+            losetup "$DEV" "$IMAGE"
+            mkfs -t $FSTYPE "$DEV"
+            losetup -d "$DEV"
+            set +e
+        fi
+        ;;
+
+    loop-mount)
+        SOURCE="$1"
+        DEST="$2"
+        #MARKER="$3"
+        [ -z "$DEST" ] && DEST="`basename $SOURCE`.mnt"
+        [ -z "$MARKER" ] && MARKER="`basename $SOURCE`.status"
+        [ -f "$SOURCE" ] || { echo "Source file missing: \"$SOURCE\"" 1>&2 ; exit 1; }
+        [ -e "$DEST" ] && { echo "Destination file exists: \"$DEST\"" 1>&2 ; exit 1; }
+        #[ -e "$MARKER" ] && { echo "Marker file exists: \"$MARKER\"" 1>&2; exit 1; }
+
+        if ! amiroot ; then
+            $SUDO "$0" "$MODE" "$@"
+        else
+            # Oh i didn't know that, how fancy:
+            # <quote source="man mount">
+            #   Since  Linux 2.6.25 is supported auto-destruction of loop devices and then any loop
+            #   device allocated by mount will be freed by umount independently on /etc/mtab.
+            # </quote>
+
+            set -e
+            mkdir "$DEST"
+            mount "$SOURCE" "$DEST" -o loop
+            #echo "#!/bin/sh" >$MARKER
+            #echo "umount \"$DEST\"" >>$MARKER
+            set +e
+        fi
+        ;;
+
+    umount)
+        DEST="$1"
+        [ -d "$DEST" ] || { echo "Destination missing or not a directory: \"$DEST\"" 1>&2 ; exit 1; }
+        
+        if ! amiroot ; then
+            $SUDO "$0" "$MODE" "$@"
+        else
+            umount "$DEST"
+        fi
+        ;;
+
+    copy-content)
+        SOURCE="$1"
+        DEST="$2"
+
+        [ -d "$SOURCE" ] || { echo "Source missing or not a directory: \"$SOURCE\"" 1>&2 ; exit 1; }
+        [ -d "$DEST" ] || { echo "Destination missing or not a directory: \"$DEST\"" 1>&2 ; exit 1; }
+
+        if ! amiroot ; then
+            $SUDO "$0" "$MODE" "$@"
+        else
+            # rsync >> cp
+            # The slash after source is important.
+            rsync -avxHAX -W -P --numeric-ids "$SOURCE/" "$DEST"
+        fi
+        ;;
+
+    info|*)
+        FMT="%18s: %s\\n"
+        #echo "$0 $MODE $@"
+        printf "$FMT" ARCH "$ARCH"
+        printf "$FMT" RELEASE "$RELEASE"
+        printf "$FMT" VARIANT "$VARIANT"
+        printf "$FMT" MIRROR "$MIRROR"
+        printf "$FMT" IMAGE_SIZE_MB "$IMAGE_SIZE_MB"
+        printf "$FMT" IMAGE_SIZE_BYTES "$IMAGE_SIZE_BYTES"
+        ;;
+
+esac
+
